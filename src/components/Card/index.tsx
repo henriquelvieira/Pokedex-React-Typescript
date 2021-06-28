b61dac3e8c0d01c1b4dfd3f975d94875774fef99
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-06-28 20:19:27 -0300

    Adição de Link ao Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import './styles.scss';
 
@@ -13,12 +14,14 @@ export function Card({
     name
   }: CardProps) {
     return (
-          <div className="card">
-            <img src={imagem} alt={name} />
-            <div className="container">
-              <h4><b>{name}</b></h4>
-              <p>Habilidades</p>
+           <div className="card">
+              <Link to={name}>
+                <img src={imagem} alt={name} />
+                <div className="container">
+                  <h4><b>{name}</b></h4>
+                  <p>Habilidades</p>
+                </div>
+              </Link>
             </div>
-          </div>
     )
 }
